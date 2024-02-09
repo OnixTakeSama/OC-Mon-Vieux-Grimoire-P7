705211dc2972cde4f3ff914ff451d5c8c7036c22
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2024-02-09 16:27:28 +0100

    Fixes
    Updating node version
    Converted images to webp with sharp

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,5 +1,6 @@
 const multer = require("multer");
 
+// On stocke l'image dans la mémoire pour la transformer avec sharp
 const storage = multer.memoryStorage()
 
 module.exports = multer({ storage }).single('image')
--- backend/middlewares/sharp-config.js
+++ backend/middlewares/sharp-config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = (req, res, next) => {
 
+    // On vérifie si un fichier est envoyé, si ce n'est pas le cas on passe à la suite
     console.log("req.file:", req.file)
     if (!req.file) {
         next()
@@ -10,7 +11,11 @@ module.exports = (req, res, next) => {
     }
 
     const { buffer, fieldname } = req.file;
+
+    // On crée un nom de fichier unique
     const filename = `${fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.webp`;
+
+    // On traite l'image avec sharp
     sharp(buffer)
         .webp()
         .toFile(path.resolve(__dirname, `../images/${filename}`), (err, info) => {
