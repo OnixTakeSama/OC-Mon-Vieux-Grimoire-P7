90eb54c25f36508247920884095fb7c9bfb0a10f
Benjamin W. <71712365+OnixTakeSama@users.noreply.github.com>
2024-04-09 19:08:39 +0200

    adding path

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./app');
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
