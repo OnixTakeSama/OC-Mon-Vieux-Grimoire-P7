9fee9a5090be2191df603d0dd964ac27729273f7
Benjamin W. <71712365+OnixTakeSama@users.noreply.github.com>
2024-04-09 19:07:18 +0200

    adding fs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const https = require('https')
+const https = require('https');
+const fs = require('fs');
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
